e1365042ef599c7b3dade45761c3064105b9b506
afonso12325 <afonso.delgado@hotmail.com>
2018-11-14 09:03:40 -0300

    Mudança do código e cena do vRep, fortemente baseada em https://sites.google.com/site/vrepjai/

--- teste_pioneer_p3dx/main.cpp
+++ teste_pioneer_p3dx/main.cpp
@@ -1,143 +1,132 @@
-/* 
- * File:   main.cpp
- * Author: Daniel de Filgueiras Gomes ( daniel.fgomes@ufpe.br )
- * This source code is licensed under the MLP2 terms (MLP2.html or https://www.mozilla.org/en-US/MPL/2.0/ )
- * This code is provided as an illustrative example of communication with vRep.
- * Department of Electronics and Systems/UFPE. ( https://www.ufpe.br/des/o-des )
- * Created on 1 de Outubro de 2018, 11:18
- */
-
-        
-#include <stdio.h>
-#include <stdlib.h>
-
-extern "C" {
-    #include "remoteApi/extApi.h"
-}
 /*
- *  scene: apiRemoto_BUMPER
-    leftJointHandle=sim.getObjectHandle("dr12_leftJoint_")
-    rightJointHandle=sim.getObjectHandle("dr12_rightJoint_")
-    bumperSensorHandle=sim.getObjectHandle("dr12_bumperForceSensor_")
-    backwardModeUntilTime=0
-    currentTime=sim.getSimulationTime()
-    result,f,t=sim.readForceSensor(bumperSensorHandle)
-    if (result>0) then
-        if (math.abs(f[2])>1) or (math.abs(f[3])>1) then
-            backwardModeUntilTime=currentTime+2 -- 2 seconds backwards
-        end
-    end
-    
-    
-    if (currentTime<backwardModeUntilTime) then
-        sim.setJointTargetVelocity(leftJointHandle,-100*math.pi/180)
-        sim.setJointTargetVelocity(rightJointHandle,-50*math.pi/180)
-    else
-        sim.setJointTargetVelocity(leftJointHandle,200*math.pi/180)
-        sim.setJointTargetVelocity(rightJointHandle,200*math.pi/180)
-    end
+    Client of V-REP simulation server (remoteApi)
+    Copyright (C) 2015  Rafael Alceste Berri rafaelberri@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-int main(int argc,char* argv[])
+// Habilite o server antes na simulação V-REP com o comando lua:
+// simExtRemoteApiStart(portNumber) -- inicia servidor remoteAPI do V-REP
+
+extern "C" {
+  #include "remoteApi/extApi.h"
+}
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) 
 {
-    int usensors[16] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-    float noDetectionDist=0.5;
-    float maxDetectionDist=0.2;
-    float detect[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    float braitenbergL[16] = {-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-    float braitenbergR[16] = {-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-    float v0 = 2;
-    float vLeft, vRight;
-    char buf[50];
-    float* detected;
-    float dist;
-    int clientID=simxStart((simxChar*)"127.0.0.1",19999,true,true,2000,5);
-    if (clientID!=-1)
-    {
-        
-        printf("Connected to remote API server\n");
-
-        // Now try to retrieve data in a blocking fashion (i.e. a service call):
-        int objectCount;
-        int* objectHandles;
-        int ret=simxGetObjects(clientID,sim_handle_all,&objectCount,&objectHandles,simx_opmode_blocking);
-        if (ret==simx_return_ok)
-            printf("Number of objects in the scene: %d\n",objectCount);
-        else
-            printf("Remote API function call returned with error code: %d\n",ret);
+  string serverIP = "127.0.0.1";
+  int serverPort = 19999;
+  int leftMotorHandle = 0;
+  float vLeft = 0;
+  int rightMotorHandle = 0;
+  float vRight = 0;
+  string sensorNome[16];
+  int sensorHandle[16];
+  
+  // variaveis de cena e movimentação do pioneer
+  float noDetectionDist=0.5;
+  float maxDetectionDist=0.2;
+  float detect[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  float braitenbergL[16]={-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+  float braitenbergR[16]={-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+  float v0=2;
+  char buf[50];
+  int clientID=simxStart((simxChar*)serverIP.c_str(),serverPort,true,true,2000,5);
+  
+  if (clientID!=-1)
+  {
+    cout << "Servidor conectado!" << std::endl;
     
-        
-        //Now retrieve streaming data (i.e. in a non-blocking fashion):
-        int startTime=extApi_getTimeInMs();
-        int mouseX;
-        unsigned char statusBumper=0;
-        
-        simxGetIntegerParameter(clientID,sim_intparam_mouse_x,&mouseX,simx_opmode_streaming); // Initialize streaming
-        
-        int motorL,motorR,bumper;
-        int i;
-        
-        //simxGetObjectHandle(clientID,"dr12_bumperForceSensor_",&bumper,simx_opmode_blocking);
-        simxGetObjectHandle(clientID,"Pioneer_p3dx_leftMotor",&motorL,simx_opmode_blocking);
-        simxGetObjectHandle(clientID,"Pioneer_p3dx_rightMotor",&motorR,simx_opmode_blocking);
-        for(int i = 0;i<16;i++){
-            sprintf(buf, "Pioneer_p3dx_ultrasonicSensor%d",i+1);
-            simxGetObjectHandle(clientID,buf,&(usensors[i]),simx_opmode_blocking);
-        }
-        for(i = 0;i<16;i++){
-            simxReadProximitySensor(clientID, usensors[i],NULL, NULL,NULL,NULL,simx_opmode_streaming); 
-        }
-        simxFloat coord[3];
-        while (extApi_getTimeDiffInMs(startTime) < 7000)
-        {
-            //ret=simxGetIntegerParameter(clientID,sim_intparam_mouse_x,&mouseX,simx_opmode_buffer); // Try to retrieve the streamed data
-            //if (ret==simx_return_ok) // After initialization of streaming, it will take a few ms before the first value arrives, so check the return code
-            //    printf("Mouse position x: %d\n",mouseX); // Mouse position x is actualized when the cursor is over V-REP's window
-            for(int i = 0;i<16;i++){
-                simxReadProximitySensor(clientID, usensors[3],NULL, coord,NULL,NULL,simx_opmode_buffer);
-                if(coord == NULL){
-                    printf("NULO");
-                }
-                else
-                printf("x= %f, y=%f, z=%f \n", coord[0],coord[1],coord[0]);
-                dist = coord[2];
-                if(dist < noDetectionDist){
-                    if(dist<maxDetectionDist){
-                        dist = maxDetectionDist;
-                    }
-                    detect[i] = 1-((dist-maxDetectionDist)/(noDetectionDist-maxDetectionDist));
-                }
-                else detect[i] = 0;
-            }
-            
-            vLeft = v0;
-            vRight = v0;
-            for(int i =0;i<16;i++){
-                        vLeft=vLeft+braitenbergL[i]*detect[i];
-                        vRight=vRight+braitenbergR[i]*detect[i];
-            }
-            //simxReadForceSensor(clientID,bumper,&statusBumper,vf,vt,simx_opmode_buffer);
-            //printf("%f %f %f\n",vf[0],vf[1],vf[2]);
-            //if((vf[2])<-4.95) {
-                //simxSetJointTargetVelocity(clientID,motorL,0,simx_opmode_oneshot);
-                //simxSetJointTargetVelocity(clientID,motorR,0,simx_opmode_oneshot);
-            //}
-            //else {
-                simxSetJointTargetVelocity(clientID,motorL,vLeft,simx_opmode_oneshot);
-                simxSetJointTargetVelocity(clientID,motorR,vRight,simx_opmode_oneshot);
-            //}
-            
-        }
-        printf("saiu");
-        // Now send some data to V-REP in a non-blocking fashion:
-        simxAddStatusbarMessage(clientID,"Hello V-REP!",simx_opmode_oneshot);
-
-        // Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
-        int pingTime;
-        simxGetPingTime(clientID,&pingTime);
-
-        // Now close the connection to V-REP:   
-        simxFinish(clientID);
+    // inicialização dos motores
+    if(simxGetObjectHandle(clientID,(const simxChar*) "Pioneer_p3dx_leftMotor",(simxInt *) &leftMotorHandle, (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
+      cout << "Handle do motor esquerdo nao encontrado!" << std::endl;  
+    else
+      cout << "Conectado ao motor esquerdo!" << std::endl;
+    
+    if(simxGetObjectHandle(clientID,(const simxChar*) "Pioneer_p3dx_rightMotor",(simxInt *) &rightMotorHandle, (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
+      cout << "Handle do motor direito nao encontrado!" << std::endl;  
+    else
+      cout << "Conectado ao motor direito!" << std::endl;
+    
+    // inicialização dos sensores (remoteApi)
+    for(int i = 0; i < 16; i++)
+    {
+      sprintf(buf, "Pioneer_p3dx_ultrasonicSensor%d",i+1);
+      
+      if(simxGetObjectHandle(clientID,(const simxChar*) buf,(simxInt *) &sensorHandle[i], (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
+	cout << "Handle do sensor " << sensorNome[i] << " nao encontrado!" << std::endl;
+      else
+      {
+        cout << "Conectado ao sensor " << sensorNome[i] << std::endl;
+	simxReadProximitySensor(clientID,sensorHandle[i],NULL,NULL,NULL,NULL,simx_opmode_streaming);
+      }
     }
-    return(0);
+    
+    // desvio e velocidade do robô
+    while(simxGetConnectionId(clientID)!=-1) // enquanto a simulação estiver ativa
+    {
+      for(int i = 0; i < 16; i++)
+      {
+	simxUChar state;
+	simxFloat coord[3];
+	
+	if (simxReadProximitySensor(clientID,sensorHandle[i],&state,coord,NULL,NULL,simx_opmode_buffer)==simx_return_ok)
+	{ 
+	  float dist = coord[2];
+	  if(state > 0 && (dist<noDetectionDist))
+	  {
+	    if(dist<maxDetectionDist)
+	    {
+	      dist=maxDetectionDist;
+	    }
+	    
+	    detect[i]=1-((dist-maxDetectionDist)/(noDetectionDist-maxDetectionDist));
+	  }
+	  else
+	    detect[i] = 0;
+	}
+	else
+	  detect[i] = 0;
+      }
+      
+      vLeft = v0;
+      vRight = v0;
+      
+      for(int i = 0; i < 16; i++)
+      {
+	vLeft=vLeft+braitenbergL[i]*detect[i];
+        vRight=vRight+braitenbergR[i]*detect[i];
+      }
+      
+      // atualiza velocidades dos motores
+      simxSetJointTargetVelocity(clientID, leftMotorHandle, (simxFloat) vLeft, simx_opmode_streaming);
+      simxSetJointTargetVelocity(clientID, rightMotorHandle, (simxFloat) vRight, simx_opmode_streaming);
+      
+      // espera um pouco antes de reiniciar a leitura dos sensores
+      extApi_sleepMs(5);
+    }
+      
+    simxFinish(clientID); // fechando conexao com o servidor
+    cout << "Conexao fechada!" << std::endl;
+  }
+  else
+    cout << "Problemas para conectar o servidor!" << std::endl;
+  return 0;
 }
